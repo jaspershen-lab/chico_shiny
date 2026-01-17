78718eaa49b4c1f5dc6e1e8bd52076119c2c4898
jaspershen <shenxt1990@outlook.com>
2026-01-17 22:51:29 +0800

    update

---
+++ server.R
@@ -0,0 +1,188 @@
+
+# load data
+# set.seed(123)
+# participants <- c("All participants", paste0("Participant_", 1:40))
+# data <- expand.grid(Weeks = 1:40, Participant = participants[-1])
+# data$Progesterone <- rnorm(nrow(data), 50, 10)
+# data$Estrogen <- rnorm(nrow(data), 100, 20)
+# data$HCG <- rnorm(nrow(data), 2000, 500)
+#
+# # Add correlation and recovery score to the simulated dataset
+# data$Correlation <-
+#   runif(nrow(data), -1, 1) # Random values between -1 and 1
+# data$Recovery_Score <-
+#   runif(nrow(data), 0, 100) # Random values between 0 and 100
+#
+# data$PValue <- rep(0.000001, nrow(data))
+# data$PValue <- rep(0.000001, nrow(data))
+
+# load("data/denmark_data2")
+# 
+# denmark_data <-
+#   denmark_data2
+# 
+# participants <- c("All participants", unique(denmark_data$subject_id2))
+# 
+# # Define molecule types and associated molecules outside the server
+# molecule_choices <- list(RNA = unique(denmark_data$Molecular_name[denmark_data$class == "RNA"]),
+#                          Protein = unique(denmark_data$Molecular_name[denmark_data$class == "Protein"]),
+#                          Metabolite = unique(denmark_data$Molecular_name[denmark_data$class == "Metabolite"]),
+#                          Cytokine = unique(denmark_data$Molecular_name[denmark_data$class == "Cytokine"]))
+# 
+# 
+# subject_color <-
+#   colorRampPalette(colors = RColorBrewer::brewer.pal(11, name = "BrBG"))(n = length(unique(denmark_data$subject_id2)))
+# 
+# names(subject_color) <-
+#   stringr::str_sort(unique(denmark_data$subject_id2), numeric = TRUE)
+
+# Server
+server <-
+  function(input, output, session) {
+    output$aboutContent <- renderUI({
+      # Convert the RMarkdown to HTML
+      tmp_about <- tempfile(fileext = ".html")
+      # rmarkdown::render("markdown/about.Rmd", output_file = tmp_about)
+      # Include the HTML content in the Shiny app
+      includeHTML("markdown/about.html")
+    })
+    
+    output$authorContent <- renderUI({
+      # Convert the RMarkdown to HTML
+      tmp_authors <- tempfile(fileext = ".html")
+      # rmarkdown::render("markdown/authors.Rmd", output_file = tmp_authors)
+      # Include the HTML content in the Shiny app
+      includeHTML("markdown/authors.html")
+    })
+    
+    # Dynamic dropdown for molecules based on molecule type
+    observe({
+      updateSelectInput(
+        session = session,
+        inputId = "molecule",
+        choices = "",
+        selected = ""
+      )
+    })
+    
+    makePlot <- reactive({
+      req(input$molecule, input$participant)
+      
+      filtered_data <-
+        if ("All participants" %in% input$participant ||
+            length(input$participant) == 0) {
+          denmark_data %>% 
+            filter(Molecular_name %in% input$molecule)
+        } else {
+          denmark_data %>% 
+            filter(subject_id2 %in% input$participant) %>% 
+            filter(Molecular_name %in% input$molecule) %>% 
+            dplyr::filter(!is.na(value))
+        }
+      
+      # color_palette <-
+      #   scales::hue_pal()(length(unique(filtered_data$Participant)))
+      
+      p <-
+        ggplot(filtered_data,
+               aes(
+                 x = g_stage,
+                 y = value
+               ))
+      
+      if (input$points) {
+        p <- p + geom_point(aes(
+          group = subject_id2,
+          color = subject_id2
+        )) 
+      }
+      
+      if (input$smooth) {
+        if(input$smooth_one){
+          p <-
+            p + 
+            geom_smooth(se = FALSE,
+                        color = "black") 
+        }else{
+          p <- p + geom_smooth(se = FALSE,
+                               aes(
+                                 group = subject_id2,
+                                 color = subject_id2
+                               ))  
+        }
+      } else {
+        p <- p + geom_line(
+          aes(
+            group = subject_id2,
+            color = subject_id2
+          )
+        ) 
+      }
+      
+      p <-
+        p + ggtitle(paste(input$moleculeType, "-", input$molecule))
+      p <- p + 
+        scale_color_manual(values = subject_color[names(subject_color) %in% filtered_data$subject_id2]) +
+        theme_bw() +
+        theme(panel.grid.minor = element_blank()) +
+        labs(x = "Gestational age (weeks)",
+             y = "Scaled intensity")
+      
+      return(p)
+    })
+    
+    output$lineChart <- renderPlotly({
+      p <- makePlot()
+      ggplotly(p) %>% layout(autosize = TRUE)
+    })
+    
+    make_table <- reactive({
+      req(input$molecule, input$participant)
+      
+      filtered_data <-
+        if ("All participants" %in% input$participant ||
+            length(input$participant) == 0) {
+          denmark_data %>% 
+            filter(Molecular_name %in% input$molecule)
+        } else {
+          denmark_data %>% 
+            filter(subject_id2 %in% input$participant) %>% 
+            filter(Molecular_name %in% input$molecule) %>% 
+            dplyr::filter(!is.na(value))
+        }
+      
+      molecule_info <-
+      filtered_data %>% 
+        dplyr::distinct(variable_id, .keep_all = TRUE) %>% 
+        select(SAM_score, SAM_FDR, Correlation, Correlation_FDR,
+               ENSEMBL, UNIPROT, SYMBOL, ENTREZID, recover_score,
+               HMDB_ID, KEGG_ID)
+      
+      return(molecule_info)
+    })
+    
+    
+    output$moleculeInfoTable <- renderDT({
+      molecule_info <- make_table()
+      datatable(molecule_info)
+    })
+    
+    output$downloadPlot <- downloadHandler(
+      filename = function() {
+        paste("plot-", Sys.Date(), ".", tolower(input$filetype), sep = "")
+      },
+      content = function(file) {
+        if (input$filetype == "PNG") {
+          png(file,
+              width = input$width * 96,
+              height = input$height * 96)
+          print(makePlot())
+          dev.off()
+        } else if (input$filetype == "PDF") {
+          pdf(file, width = input$width, height = input$height)
+          print(makePlot())
+          dev.off()
+        }
+      }
+    )
+  }
---
+++ ui.R
@@ -0,0 +1,218 @@
+# Required Libraries
+# install.packages(c("shiny", "shinydashboard", "ggplot2", "dplyr", "RColorBrewer", "plotly"))
+library(shiny)
+library(shinydashboard)
+library(ggplot2)
+library(dplyr)
+library(RColorBrewer)
+library(plotly)
+library(DT)
+
+# load data
+# set.seed(123)
+# participants <- c("All participants", paste0("Participant_", 1:40))
+# data <- expand.grid(Weeks = 1:40, Participant = participants[-1])
+# data$Progesterone <- rnorm(nrow(data), 50, 10)
+# data$Estrogen <- rnorm(nrow(data), 100, 20)
+# data$HCG <- rnorm(nrow(data), 2000, 500)
+#
+# # Add correlation and recovery score to the simulated dataset
+# data$Correlation <-
+#   runif(nrow(data), -1, 1) # Random values between -1 and 1
+# data$Recovery_Score <-
+#   runif(nrow(data), 0, 100) # Random values between 0 and 100
+#
+# data$PValue <- rep(0.000001, nrow(data))
+
+# load("data/denmark_data2")
+
+# denmark_data <-
+#   denmark_data2
+# 
+# participants <- c("All participants", unique(denmark_data$subject_id2))
+# 
+# # Define molecule types and associated molecules outside the server
+# molecule_choices <- list(
+#   RNA = unique(denmark_data$Molecular_name[denmark_data$class == "RNA"]),
+#   Protein = unique(denmark_data$Molecular_name[denmark_data$class == "Protein"]),
+#   Metabolite = unique(denmark_data$Molecular_name[denmark_data$class == "Metabolite"]),
+#   Cytokine = unique(denmark_data$Molecular_name[denmark_data$class == "Cytokine"])
+# )
+# 
+# 
+# subject_color <-
+#   colorRampPalette(colors = RColorBrewer::brewer.pal(11, name = "BrBG"))(n = length(unique(denmark_data$subject_id2)))
+# 
+# names(subject_color) <-
+#   stringr::str_sort(unique(denmark_data$subject_id2), numeric = TRUE)
+
+
+# UI
+ui <- dashboardPage(
+  skin = "purple",
+  dashboardHeader(title = "CHICO Study"),
+  dashboardSidebar(sidebarMenu(
+    menuItem("About", tabName = "about", icon = icon("info-circle")),
+    menuItem(
+      "Data Visualization",
+      tabName = "viz",
+      icon = icon("line-chart")
+    ),
+    menuItem("Authors", tabName = "authors", icon = icon("users"))
+  )),
+  
+  dashboardBody(
+    tags$head(tags$style(
+      HTML(
+        "
+                /* Change the background color of the header */
+                .skin-blue .main-header .navbar,
+                .skin-blue .main-header .logo {
+                    background-color: #00458A;  /* NTU Blue */
+                    border-bottom-color: #8E0C3A;  /* NTU Red */
+                }
+
+                /* Change the hover color of the sidebar menu */
+                .skin-blue .sidebar-menu > li:hover > a,
+                .skin-blue .sidebar-menu > li.active > a {
+                    border-left-color: #8E0C3A;  /* NTU Red */
+                }
+
+                /* Other custom styles can be added as needed */
+            "
+      )
+    )),
+    tabItems(
+      # About tab
+      tabItem(tabName = "about", uiOutput("aboutContent")),
+      
+      # tabItem(tabName = "about", fluidPage(
+      #   titlePanel(""), fluidRow(column(
+      #     12, includeHTML("markdown/about.html")
+      #   ))
+      # )),
+      
+      # Data Visualization tab
+      tabItem(tabName = "viz", fluidRow(
+        column(
+          width = 3,
+          box(
+            title = "Plot Controls",
+            selectInput(
+              "participant",
+              "Participants:",
+              choices = "",
+              selected = "All participants",
+              multiple = TRUE
+            ),
+            selectInput(
+              inputId = "moleculeType",
+              label = "Molecule class:",
+              choices = "",
+              selected = "Protein"
+            ),
+            selectInput(
+              inputId = "molecule",
+              label = "Molecule:",
+              choices = "",
+              selected = ""
+            ),
+            checkboxInput("smooth", "Smooth lines?", TRUE),
+            checkboxInput("smooth_one", "Only one smoothed line?", TRUE),
+            checkboxInput("points", "Show points?", TRUE),
+            numericInput(
+              "width",
+              "Download Width (in):",
+              10,
+              min = 1,
+              max = 20
+            ),
+            numericInput(
+              "height",
+              "Download Height (in):",
+              6,
+              min = 1,
+              max = 20
+            ),
+            selectInput("filetype", "File Type:", choices = c("PNG", "PDF")),
+            downloadButton("downloadPlot", "Download Plot"),
+            width = NULL
+          )
+        ), column(
+          width = 9,
+          box(plotlyOutput("lineChart"), width = NULL),
+          box(DTOutput("moleculeInfoTable"), width = NULL)
+        )
+      )),
+      # Authors tab
+      tabItem(tabName = "authors", uiOutput("authorContent"))
+    ),
+    tags$head(tags$style(
+      HTML(
+        "
+        .author-img {
+          border-radius: 50%;
+          width: 120px;
+          height: 150px;
+          object-fit: cover;
+        }
+      "
+      )
+    )),
+    tags$footer(
+      div(
+        style = "background-color: #ecf0f4; display: flex; align-items: center; justify-content: left; padding: 10px; height: 80px; position: fixed; bottom: 0; width: 100%; z-index: 100; border-top: 1px solid #ccc;",
+        tags$img(
+          src = "ntu_logo.png",
+          height = "80px",
+          style = "margin-right: 15px;",
+          onerror = "this.onerror=null; this.src='default_logo.png';"
+        ),
+        # Add vertical line
+        div(style = "border-left: 1px solid #ccc; height: 50px; margin-right: 15px;"),
+        tags$img(
+          src = "SUSig_Red_Stree_Stacked_Left.png",
+          height = "80px",
+          style = "margin-right: 15px;",
+          onerror = "this.onerror=null; this.src='default_logo.png';"
+        ),
+        # Add vertical line
+        div(style = "border-left: 1px solid #ccc; height: 50px; margin-right: 15px;"),
+        tags$img(
+          src = "CHICO_logo.png",
+          height = "60px",
+          style = "margin-right: 15px;",
+          onerror = "this.onerror=null; this.src='default_logo.png';"
+        ),
+        # Add vertical line
+        div(style = "border-left: 1px solid #ccc; height: 50px; margin-right: 15px;"),
+        div(
+          # HTML("The Shen Lab at Nanyang Technological University Singapore"),
+          # HTML("<br>"),
+          tags$a(
+            href = "http://www.shen-lab.org",
+            target = "_blank",
+            tags$i(class = "fa fa-house", style = "color: purple;"),
+            " Shen Lab",
+            style = "text-align: left; margin-left: 10px;"
+          ),
+          tags$a(
+            href = "https://www.shen-lab.org/#contact",
+            target = "_blank",
+            tags$i(class = "fa fa-envelope", style = "color: purple;"),
+            " Email",
+            style = "text-align: left; margin-left: 10px;"
+          ),
+          tags$a(
+            href = "https://github.com/jaspershen-lab",
+            target = "_blank",
+            tags$i(class = "fa fa-github", style = "color: purple;"),
+            " GitHub",
+            style = "text-align: left; margin-left: 10px;"
+          ),
+          style = "text-align: left;"
+        )
+      )
+    )
+  )
+)
